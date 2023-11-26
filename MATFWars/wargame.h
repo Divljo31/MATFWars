fbf206d2f6cd8b6054dea073ca077bbf963cdc02
Andrea <djukanovic.an@gmail.com>
2023-11-26 17:51:54 +0100

    fixed class WarGame and deleted WarGame.h and Canvas.h

--- MATFWars/wargame.h
+++ MATFWars/wargame.h
@@ -2,6 +2,9 @@
 #define WARGAME_H
 
 #include <QDialog>
+#include <QVector>
+#include "PlayerWar.h"
+#include "Obstacle.h"
 
 namespace Ui {
 class WarGame;
@@ -14,9 +17,28 @@ class WarGame : public QDialog
 public:
     explicit WarGame(QWidget *parent = nullptr);
     ~WarGame();
+    static bool bothConnected;
+    static bool startGame();
+    static bool addPlayer();
+    static bool removePlayer();
+    static bool endGame();
+
+    QVector<Obstacle> generateObstacles(int x, int y);
+    QPointF randomPoint(int x, int y);
+
+
+    PlayerWar player1() const;
+
+    PlayerWar player2() const;
+
+    Obstacle getObstacle(size_t index) const;
+
 
 private:
     Ui::WarGame *ui;
+    PlayerWar m_player1;
+    PlayerWar m_player2;
+    QVector<Obstacle> m_obstacles;
 };
 
 #endif // WARGAME_H
