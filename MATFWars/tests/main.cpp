31f61a9e501601786b90b79fff3d3f5249e7f46e
miletaj <mileta.jovanovic01@gmail.com>
2023-12-27 14:54:10 +0100

    catch2 and function tests start

--- MATFWars/tests/main.cpp
+++ MATFWars/tests/main.cpp
@@ -1,9 +1,10 @@
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
-#include <QApplication>
+#include <QCoreApplication>
 
-int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
     int result = Catch::Session().run(argc,argv);
     return result;
 }
